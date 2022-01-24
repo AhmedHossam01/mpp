1f02696a20b2364c5779eca75cc93c6af52f5b35
Ahmed Hossam <1318039@gmail.com>
2022-01-24 21:34:54 +0200

    Add feature: clost menu when clicking on a link

--- menu.js
+++ menu.js
@@ -1,10 +1,16 @@
-const buttonMobile = document.getElementById('btn-menu')
-const closingButton = document.getElementById('close')
+const buttonMobile = document.getElementById("btn-menu");
+const closingButton = document.getElementById("close");
+const navbarLink = document.querySelector(".navbar__link");
 
 function toggleMenu() {
-    const nav = document.getElementById('nav')
-    nav.classList.toggle('navbar__menu--open');
+  console.log("clicked link");
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("navbar__menu--open");
 }
 
-buttonMobile.addEventListener('click', toggleMenu)
-closingButton.addEventListener('click', toggleMenu)
+buttonMobile.addEventListener("click", toggleMenu);
+closingButton.addEventListener("click", toggleMenu);
+
+document.querySelectorAll(".navbar__link").forEach((item) => {
+  item.addEventListener("click", toggleMenu);
+});
