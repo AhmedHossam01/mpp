569a7b7a16edb7027cdfd4151a860620806ce50a
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-01-25 16:35:02 -0300

    almost finished modal style

--- projects.js
+++ projects.js
@@ -16,7 +16,7 @@ const projects = [
     position: "Back End Dev",
     date: "2015",
     desc: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
     languages: ["html", "css", "javascript"],
     imageUrl: "images/project2.png",
     id: "2"
@@ -68,30 +68,40 @@ function generateHTML(details) {
           <div class="modal" id="modal">
             <div class="modal-content">
               <div class="modal-close">
-                <button class="modal-close">Close</button>
+                <button class="modal-close"><img src="./images/closing_btn.svg" alt="Closing Button" id="close"></button>
               </div>
-              <div class="works__single__details">
+              <div class="works__single__holder">
+                <h2 class="works__single__title">${details.title}</h2>
+          
+                <div class="works__single__details">
                 <p class="works__single__details__single works__single__details__single--dark">
-                  ${details.company}
+                    ${details.company}
                 </p>
-                <div class="dot">
-                </div>
+                <div class="dot"></div>
                 <p class="works__single__details__single works__single__details__single--light">
                 ${details.position}
                 </p>
                 <div class="dot"></div>
                 <p class="works__single__details__single works__single__details__single--light">
-                  ${details.date}
+                ${details.date}
                 </p>
               </div>
               <div class="works__single__img_container">
                 <img class="works__single__img" src="${details.imageUrl}" alt="${details.title}" />
               </div>
               <div>
-                <p>"${details.body}"</p>
+                <p>${details.body}</p>
               </div>
-              <div class="works__single__tags">
-                tags
+              <div class="works__single__tags hr">
+                <div class="works__single__tags">
+                <div class="works__single__tags__single">${details.languages[0]}</div>
+                <div class="works__single__tags__single">${details.languages[1]}</div>
+                <div class="works__single__tags__single">${details.languages[2]}</div>
+              </div>
+              </div>
+              <div class="modal-links">
+                <a href="./index.html"><button class="button">See Live <img src="./images/see-live.svg" alt="See Live"></button></a>
+                <a href="./index.html"><button class="button">See Source <img src="./images/see-source.svg" alt="See Live"></button></a>
               </div>
             </div>
           </div>
