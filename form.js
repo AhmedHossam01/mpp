530fc39131e91cd0b5315cfa0f271da862bfdc65
Ahmed Hossam <1318039@gmail.com>
2022-01-26 23:20:12 +0200

    remove console.log

--- form.js
+++ form.js
@@ -4,8 +4,6 @@ const errorMessage = document.querySelector(".error-message");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("submit form");
-  console.log(email.value.toLowerCase());
 
   if (email.value !== email.value.toLowerCase()) {
     errorMessage.classList.replace("d-none", "d-block");
