ef724161ad6fea4e7dfb947e8130a53058c400ed
Ahmed Hossam <1318039@gmail.com>
2022-01-26 22:32:40 +0200

    Implement email validation

---
+++ form.js
@@ -0,0 +1,15 @@
+const form = document.querySelector(".contact-form");
+const email = document.querySelector("#_replyto");
+const errorMessage = document.querySelector(".error-message");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("submit form");
+  console.log(email.value.toLowerCase());
+
+  if (email.value !== email.value.toLowerCase()) {
+    errorMessage.classList.replace("d-none", "d-block");
+  } else {
+    errorMessage.classList.replace("d-block", "d-none");
+  }
+});
